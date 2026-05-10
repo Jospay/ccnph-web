555499d26b9ed8ff1bd3497e615559fc9018d70e
joshi <jespadilla422@gmail.com>
2026-05-10 13:36:32 +0800

    feat(profile): add phone, birthdate, gender, and apply inertia useForm

--- resources/js/types/auth.ts
+++ resources/js/types/auth.ts
@@ -10,6 +10,8 @@ export type User = {
   name: string;
   email: string;
   avatar?: string;
+  phone: string | null;
+  birthdate: string | null;
   email_verified_at: string | null;
   created_at: string;
   updated_at: string;
