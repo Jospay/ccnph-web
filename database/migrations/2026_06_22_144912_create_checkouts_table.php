8cb624008bcfacd7a7a8ba99d8cb59d16c7931ad
joshi <jespadilla422@gmail.com>
2026-06-22 16:59:41 +0800

    feat(migration): add checkouts, orders, and items tables

---
+++ database/migrations/2026_06_22_144912_create_checkouts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('checkout_number')->unique();
+            $table->decimal('grand_total', 12, 2);
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkouts');
+    }
+};
---
+++ database/migrations/2026_06_22_145228_create_orders_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\OrderStatus;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('shop_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('checkout_id')->constrained()->cascadeOnDelete();
+            $table->string('order_number')->unique();
+            $table->string('status')->default(OrderStatus::PENDING->value);
+            $table->decimal('subtotal', 12, 2);
+            $table->decimal('shipping_fee', 12, 2)->default(0);
+            $table->decimal('discount', 12, 2)->default(0);
+            $table->decimal('total', 12, 2);
+            $table->text('notes')->nullable();
+            // Shipping Address Reference
+            $table->string('recipient_name');
+            $table->string('recipient_phone');
+            $table->string('region');
+            $table->string('province');
+            $table->string('city');
+            $table->string('barangay');
+            $table->string('street');
+            $table->string('unit_bldg_house');
+            $table->string('postal_code');
+            $table->text('landmark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
