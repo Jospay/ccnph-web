83fcb1aafcb5bed2efd248ba46aaadab7be0bfe4
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-07-10 15:06:32 +0800

    etc

--- routes/channels.php
+++ routes/channels.php
@@ -1,17 +1,33 @@
 <?php
 
 use App\Models\Conversation;
+use App\Models\ShopConversation;
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+// For Intellectual Property Conversations
+Broadcast::channel('conversation.{conversationId}', function ($user, $conversationId) {
+    $conversation = Conversation::find($conversationId);
+
+    if (!$conversation) {
+        return false;
+    }
+
+    return Gate::forUser($user)->allows('view', $conversation);
+});
 
 Broadcast::channel('App.Models.User.{id}', function (User $user, int $id) {
     return (int) $user->id === $id;
 });
 
-Broadcast::channel('conversation.{conversationId}', function (User $user, $conversationId) {
-    $conversation = Conversation::find($conversationId);
+// For Shop Conversations
+Broadcast::channel('shop-conversation.{conversationId}', function ($user, $conversationId) {
+    $conversation = ShopConversation::find($conversationId);
 
-    if (! $conversation) {
     if (! $conversation) {
         return false;
     }
