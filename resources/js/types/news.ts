087ffba5427123e67ae37e19169e4d4b03960f9b
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-09-09 00:52:24 +0800

    feat: create migration for news tables and define NewsItem interface

---
+++ resources/js/types/news.ts
@@ -0,0 +1,8 @@
+export interface NewsItem {
+    id: number | string;
+    PostTitle: string;
+    PostImage: string;
+    PostDetails: string;
+    CategoryName: string;
+    PostingDate: string;
+}
